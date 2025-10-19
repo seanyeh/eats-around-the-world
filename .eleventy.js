f4cf529430c755780613514c0699d85ac027b4ae
Sean Yeh <109418+seanyeh@users.noreply.github.com>
2025-10-19 15:48:26 -0500

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+import * as sass from "sass";
+
+export default function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images");
+  
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function(inputContent, inputPath) {
+      let result = sass.compileString(inputContent, {
+        loadPaths: [
+          this.config.dir.input + "/_includes",
+          this.config.dir.input
+        ]
+      });
+      return async () => {
+        return result.css;
+      };
+    }
+  });
+  
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    },
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "liquid"
+  };
+}
